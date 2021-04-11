52950a6b69adee55ba1bde3936144102f236f0a2
naaa187 <knana18@ewhain.net>
2021-04-11 13:47:52 +0900

    :sparkles: feat : add market map&reg

--- src/app/nana_MarketMap.tsx
+++ src/app/nana_MarketMap.tsx
@@ -1,150 +1,92 @@
-// import { GoogleMap, useJsApiLoader } from '@react-google-maps/api'
-// import { useCallback, useState } from 'react'
-// import { shallowEqual, useSelector } from 'react-redux'
-// import { useNavigate } from 'react-router'
-// import { RootState } from 'src/app/rootReducer'
-// import { Button } from 'src/components/buttons/Button'
-// import { Box } from 'src/components/layout/Box'
-// import { ScreenContentFrame } from 'src/components/layout/ScreenContentFrame'
-// import { MoneyValue } from 'src/components/MoneyValue'
-// import { Currency } from 'src/currency'
-// import { Font } from 'src/styles/fonts'
-// import { mq, useIsMobile } from 'src/styles/mediaQueries'
-// import { Stylesheet } from 'src/styles/types'
+import * as React from 'react'
+import { useNavigate } from 'react-router'
+import { Button } from 'src/components/buttons/Button'
+import SearchIcon from 'src/components/icons/search.svg'
+import { Box } from 'src/components/layout/Box'
+import SimpleSlider from 'src/components/layout/nana_MarketSlider'
+import { mq, useIsMobile } from 'src/styles/mediaQueries'
+import { Stylesheet } from 'src/styles/types'
 
-// const containerStyle = {
-//   width: '400px',
-//   height: '400px',
-// }
+export function MarketMap() {
+  const isMobile = useIsMobile()
+  const navigate = useNavigate()
 
-// const center = {
-//   lat: -3.745,
-//   lng: -38.523,
-// }
+  const onClickViewList = () => {
+    navigate('/MarketList')
+  }
 
-// export function MyComponent() {
-//   const isMobile = useIsMobile()
-//   const navigate = useNavigate()
+  const nfts = [
+    {
+      tokenId: 1,
+      imageUrl: '../static/nft/bigdog.png',
+    },
+    {
+      tokenId: 2,
+      imageUrl: '../static/nft/puppy.png',
+    },
+    {
+      tokenId: 3,
+      imageUrl: '../static/nft/cat.png',
+    },
+  ]
 
-//   const onClickSellerLogin = () => {
-//     navigate('/seller-login')
-//   }
+  const settings = {
+    className: 'center',
+    centerMode: true,
+    infinite: true,
+    centerPadding: '60px',
+    slidesToShow: 3,
+    speed: 500,
+  }
 
-//   const balances = useSelector((s: RootState) => s.wallet.balances, shallowEqual)
+  return (
+    <Box direction="column" styles={style.frame}>
+      <Box styles={style.search}>
+        <img width="16.5pt" src={SearchIcon} style={{ padding: '0 0.2em' }} />
+        <input
+          name="search"
+          value="Pick your market"
+          style={{ padding: '0 0.3em', border: 'none' }}
+        />
+      </Box>
+      <SimpleSlider />
+      <Button onClick={onClickViewList} styles={style.button}>
+        View the list of this markets
+      </Button>
+    </Box>
+  )
+}
 
-//   const { isLoaded } = useJsApiLoader({
-//     id: 'google-map-script',
-//     googleMapsApiKey: 'AIzaSyBcalMGfbTYhGElntUWnHU7wIbx5PFYlIE',
-//   })
-
-//   const [map, setMap] = useState(null)
-
-//   const onLoad = useCallback(function callback(map) {
-//     const bounds = new window.google.maps.LatLngBounds()
-//     map.fitBounds(bounds)
-//     setMap(map)
-//   }, [])
-
-//   const onUnmount = useCallback(function callback(map) {
-//     setMap(null)
-//   }, [])
-
-//   return isLoaded ? (
-//     <ScreenContentFrame>
-//       <Box direction="column" justify="center" align="center">
-//         <h1 css={style.h1}>{`My ${isMobile ? '' : 'simple '}Page`}</h1>
-//         <h2 css={style.h2}>Your Account Balance</h2>
-//       </Box>
-//       <Box direction="column" align="center" justify="center" wrap={true} css={style.balances}>
-//         <MoneyValue
-//           amountInWei={balances.cUsd}
-//           currency={Currency.cUSD}
-//           roundDownIfSmall={true}
-//           baseFontSize={1.4}
-//           containerCss={style.balanceContainer}
-//         />
-//         <MoneyValue
-//           amountInWei={balances.celo}
-//           roundDownIfSmall={true}
-//           currency={Currency.CELO}
-//           containerCss={style.balanceContainer}
-//           baseFontSize={1.4}
-//         />
-//       </Box>
-//       <div css={style.buttonContainer}>
-//         <h2 css={style.h2}>Are you a seller?</h2>
-//         <Button
-//           size="s"
-//           onClick={onClickSellerLogin}
-//           margin="0.5em 1em"
-//           styles={{ fontSize: '1.1em' }}
-//         >
-//           Seller Login
-//         </Button>
-//         <GoogleMap
-//           mapContainerStyle={containerStyle}
-//           center={center}
-//           zoom={10}
-//           onLoad={onLoad}
-//           onUnmount={onUnmount}
-//         >
-//           {/* Child components, such as markers, info windows, etc. */}
-//           <></>
-//         </GoogleMap>
-//       </div>
-//     </ScreenContentFrame>
-//   ) : (
-//     <></>
-//   )
-// }
-
-// const style: Stylesheet = {
-//   h1: {
-//     ...Font.h1,
-//     ...Font.bold,
-//     fontSize: '1.5em',
-//     margin: '0.5em 0.5em 2em 0.5em',
-//     textAlign: 'center',
-//     maxWidth: '95%',
-//     [mq[768]]: {
-//       fontSize: '1.5em',
-//       marginTop: '0.5em',
-//     },
-//   },
-//   h2: {
-//     ...Font.h2,
-//     fontSize: '1.1em',
-//     margin: 0,
-//     opacity: 0.9,
-//     textAlign: 'center',
-//     maxWidth: '92%',
-//     [mq[768]]: {
-//       fontSize: '1.3em',
-//     },
-//   },
-//   buttonContainer: {
-//     marginTop: '1em',
-//     display: 'flex',
-//     flexDirection: 'column',
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//     [mq[768]]: {
-//       marginTop: '1.5em',
-//       flexDirection: 'row',
-//     },
-//   },
-//   balances: {
-//     letterSpacing: '0.05em',
-//     margin: '1.5em 0em',
-//   },
-//   balanceContainer: {
-//     margin: '0.5em 0.5em',
-//     fontSize: '0.9em',
-//     [mq[480]]: {
-//       fontSize: '1em',
-//     },
-//     [mq[768]]: {
-//       margin: '0 1.6em',
-//     },
-//   },
-// }
+const style: Stylesheet = {
+  frame: {
+    minHeight: '100vh',
+    backgroundImage: `url("static/maps.jpg")`,
+  },
+  search: {
+    position: 'absolute',
+    top: '6em',
+    left: '50%',
+    marginLeft: '-44vw',
+    padding: '1em 1em',
+    width: '88vw',
+    height: '3.5em',
+    background: 'white',
+    borderRadius: 10,
+    boxShadow: '0px 0px 10px #ccc',
+  },
+  button: {
+    position: 'absolute',
+    bottom: '3.75em',
+    left: '50%',
+    marginLeft: '-8em',
+    width: '18em',
+    minWidth: '15em',
+    height: '2.8em',
+    background: 'linear-gradient(0.25turn,rgba(247,214,55),rgba(47,207,87))',
+    borderRadius: 50,
+    fontSize: '1.1em',
+    [mq[768]]: {
+      marginTop: '1.5em',
+    },
+  },
+}
